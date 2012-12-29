e02f9a9d12dad1c4558012364a85f5f29c7b46b3
steve1066d <steve1066d@7a284aae-4151-4fad-a3af-e7759488fb33>
2012-12-29 19:15:25 +0000

    Add scaling slider Add dragging to reposition image in viewer. Add DestinationDPI operationList Removed much of the old viewer prescale code, and simplified scaling. Added preference dialog, (but not enabled yet)

---
+++ src/net/sourceforge/bookscanwizard/SaveOperation.java
@@ -0,0 +1,12 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.sourceforge.bookscanwizard;
+
+/**
+ * A marker interface that indicates that an operation saves a file.
+ */
+public interface SaveOperation {
+    
+}
